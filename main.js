d0f9c068149dd9f0b31cbdd346a0920f9f72a5b9
Shaun Emerson <109479721+ShaunEmerson@users.noreply.github.com>
2023-05-14 19:56:52 -0600

    Add grid layout to container, and tie functionity to buttons so that grid size can be changed. Add color changing ability to the divs inside the container div to create a drawing effect when mouseover event captured.

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-const div = document.createElement('div');
-const lButton = document.getElementById('l-button');
-const rButton = document.getElementById('r-button');
+let lButton = document.getElementById('l-button');
+let rButton = document.getElementById('r-button');
+let container = document.querySelector('#container');
 let gridNumber = 16;
+let gridNumInt = parseInt(gridNumber);
+let gridSize = (gridNumInt * gridNumInt);
 
 const validate = function(item) {
     if (item < 16) {
@@ -10,18 +12,67 @@ const validate = function(item) {
     if (item > 100) {
         item = 100;
     };
+
     while (Number.isInteger(item) === false) {
         item = parseInt(prompt('Invalid selection! Please choose a grid size between 16 & 100', 16));
     };
 }
 
+const auto = function () {
+    gridNumInt = parseInt(gridNumber);
+    let autoNum = " auto";
+    for (let i = 1 ; i < gridNumInt ; i++) {
+        autoNum += " auto";
+    }
+    return autoNum;
+}
+
+let styleCont = function () {
+    container.style=`grid-template-columns: ${auto()};`;
+}
+
+const createGrid = function () {
+    for (let i = 1 ; i <= gridSize; i++) {
+        let div = document.createElement('div');
+        div.setAttribute('class', 'pixel');
+        container.appendChild(div);
+    }
+    styleCont();
+    const pixels = document.querySelectorAll('.pixel');
+    pixels.forEach((pixel) => {
+    pixel.addEventListener('mouseover', function(){
+        this.style.background = "black";
+        let opacity = this.style.opacity;
+        this.style.opacity = opacity ? (parseFloat(opacity) + 0.2) : 0.4;
+        })
+    });
+}
+
+createGrid();
+
+const eraseGrid = function () {
+        container.innerHTML="";
+}
+
+
 lButton.addEventListener('click', () => {
-    gridNumber = parseInt(prompt('Choose a grid size between 16 & 100', 16));
-    validate(gridNumber);
+    gridNumber = prompt('Choose a grid size between 16 & 100', `${gridNumInt}`);
+    if (gridNumber === null) {
+        return;
+    }
+    eraseGrid();
+    gridNumInt = parseInt(gridNumber);
+    validate(gridNumInt);
+    gridSize = (gridNumInt * gridNumInt);
+    createGrid();
 });
 
 const reset = rButton.addEventListener('click', () => {
-    window.location.reload();
+    eraseGrid();
+    createGrid();
 });
 
 
+/*element.addEventListener("mouseover",function(){
+    this.style.background = "#0000ff";
+});*/
