27333a22e41fe0d728a3ad2b6d54600b0ed920bb
Shaun Emerson <109479721+ShaunEmerson@users.noreply.github.com>
2023-05-18 18:05:54 -0600

    Add div, buttons, and color backgrounds to create a sheet of paper with selectable colors for changing the drawing color. Add functionality to the buttons in main.js so that the buttons change the color.

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
+
+let color = "black";
+/*
+const black = document.getElementById('black');
+const green = document.getElementById('green');
+const orange = document.getElementById('orange');
+const purple = document.getElementById('purple');
+const red = document.getElementById('red');
+const white = document.getElementById('white');
+const yellow = document.getElementById('yellow');
+*/
 let lButton = document.getElementById('l-button');
 let rButton = document.getElementById('r-button');
 let container = document.querySelector('#container');
-let gridNumber = 16;
+let gridNumber = 50;
 let gridNumInt = parseInt(gridNumber);
 let gridSize = (gridNumInt * gridNumInt);
 
@@ -18,6 +29,8 @@ const validate = function(item) {
     };
 }
 
+/* Creates a number of grid columns based on the user input
+for use in styleCont function */
 const auto = function () {
     gridNumInt = parseInt(gridNumber);
     let autoNum = " auto";
@@ -31,6 +44,7 @@ let styleCont = function () {
     container.style=`grid-template-columns: ${auto()};`;
 }
 
+
 const createGrid = function () {
     for (let i = 1 ; i <= gridSize; i++) {
         let div = document.createElement('div');
@@ -38,10 +52,10 @@ const createGrid = function () {
         container.appendChild(div);
     }
     styleCont();
-    const pixels = document.querySelectorAll('.pixel');
+    pixels = document.querySelectorAll('.pixel');
     pixels.forEach((pixel) => {
     pixel.addEventListener('mouseover', function(){
-        this.style.background = "black";
+        this.style.background = `${color}`;
         let opacity = this.style.opacity;
         this.style.opacity = opacity ? (parseFloat(opacity) + 0.2) : 0.4;
         })
@@ -50,8 +64,16 @@ const createGrid = function () {
 
 createGrid();
 
+const btns = document.querySelectorAll('button');
+btns.forEach((button) => {
+    button.addEventListener('click', () => {
+        color = button.id;
+    });
+});
+
+
 const eraseGrid = function () {
-        container.innerHTML="";
+    container.innerHTML="";
 }
 
 
@@ -71,8 +93,3 @@ const reset = rButton.addEventListener('click', () => {
     eraseGrid();
     createGrid();
 });
-
-
-/*element.addEventListener("mouseover",function(){
-    this.style.background = "#0000ff";
-});*/
